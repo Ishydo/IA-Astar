a5746b75e0faad86583c1992f395cd0e825f98d2
dominiqu.ducommun <dominique.ducommun@he-arc.ch>
2016-11-24 17:24:22 +0100

    Add full test mode + heuristic function passed in astar search params)

--- TP_Astar.py
+++ TP_Astar.py
@@ -7,7 +7,8 @@ import operator
 # ASTAR CONFIGURATION #################################################
 SOURCE_CITY = "Warsaw"      # Source city
 TARGET_CITY = "Lisbon"      # Desination city
-HEURISTIC_FUNCTION = h4     # Heuristic function
+TEST_ALL_HEURISTIC = True   # Choisir si exécute toutes les heuristiques ou celle d'en dessous
+HEURISTIC_FUNCTION = h4     # Heuristic function [h0, h1, h2, h3, h4]
 PRINT_STEPS_MODE = True     # Print frontiere + history each iteration
 ######################################################################
 
@@ -15,7 +16,7 @@ cities = []
 cities_dict = {}
 
 #Astar method
-def astar_search(sourceCityState, targetCity):
+def astar_search(sourceCityState, targetCity, h_func):
 
     iterations = 0 # Pour les observations
 
@@ -26,9 +27,6 @@ def astar_search(sourceCityState, targetCity):
     while frontiere:
 
         iterations += 1 # Une iteration en plus
-
-        openstates = set()
-        openstatesf = set()
 
         cityState = frontiere.pop() # On récupère le tout premier état
 
@@ -49,9 +47,19 @@ def astar_search(sourceCityState, targetCity):
 
         possibleDestinations = cityState.possible_destinations() # Si la ville n'est pas trouvée on récupère les destinations possibles (voisins directs de la ville visitée)
         for possibleCity in possibleDestinations: # Pour chacune des destinations possibles, on crée un nouvel état
-            newCityState = cityState.apply(possibleCity[1], HEURISTIC_FUNCTION(possibleCity[1], targetCity), possibleCity[0]) # Nouvel état avec la nouvelle ville à visiter, l'heuristique et la distance
+            newCityState = cityState.apply(possibleCity[1], h_func(possibleCity[1], targetCity), possibleCity[0]) # Nouvel état avec la nouvelle ville à visiter, l'heuristique et la distance
             if (newCityState not in history) and newCityState.legal(): # Si cet état n'est pas dans l'historique alors on l'ajoute dans frontière
-                frontiere.append(newCityState)
+
+                # Pour éviter les doublons dans la frontière
+                present = False
+                for regState in frontiere:
+                    if regState.city.name == newCityState.city.name:
+                        present = True
+                        if newCityState.f < regState.f:
+                            frontiere.remove(regState)
+                            frontiere.append(newCityState)
+                if(not present):
+                    frontiere.append(newCityState)
 
         # Finalement on trie la frontière en fonction de l'attribut f de l'état (f = h + g)
         frontiere = sorted(frontiere, key=operator.attrgetter('f'), reverse=True)
@@ -69,8 +77,22 @@ if __name__ == '__main__':
     sourceCity = cities_dict[SOURCE_CITY]
     targetCity = cities_dict[TARGET_CITY]
 
-    # Récupération du résultat avec a*
-    astar_result = astar_search(State(sourceCity, HEURISTIC_FUNCTION(sourceCity, targetCity)), targetCity)
+    funcDict = {0: h0, 1: h1, 2:h2, 3:h3, 4:h4}
 
-    # Affichage de la réponse avec chemin
-    print_result(astar_result)
+    if TEST_ALL_HEURISTIC:
+        for k, hfunc in funcDict.items():
+            print("--------------------------------------------")
+            print("------------- Execution en h" + str(k) + " --------------")
+            print("--------------------------------------------")
+
+            # Récupération du résultat avec a*
+            astar_result = astar_search(State(sourceCity, hfunc(sourceCity, targetCity)), targetCity, hfunc)
+
+            # Affichage de la réponse avec chemin
+            print_result(astar_result)
+    else: # N'exécute qu'avec l'heuristique définie
+        # Récupération du résultat avec a*
+        astar_result = astar_search(State(sourceCity, HEURISTIC_FUNCTION(sourceCity, targetCity)), targetCity, HEURISTIC_FUNCTION)
+
+        # Affichage de la réponse avec chemin
+        print_result(astar_result)
